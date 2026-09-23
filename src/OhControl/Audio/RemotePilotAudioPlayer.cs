4e8953e2364487a4cfed3d840762f17c52a8545c
Noskarant <tutomod@gmail.com>
2026-09-23 18:50:45 +0200

    Add remote pilot radio playback

---
+++ src/OhControl/Audio/RemotePilotAudioPlayer.cs
@@ -0,0 +1,77 @@
+using System;
+using NAudio.Wave;
+
+namespace OhControl.Audio
+{
+    public sealed class RemotePilotAudioPlayer :
+        IDisposable
+    {
+        private readonly object _gate =
+            new object();
+
+        private readonly BufferedWaveProvider _buffer;
+        private readonly WaveOutEvent _output;
+
+        public RemotePilotAudioPlayer()
+        {
+            _buffer =
+                new BufferedWaveProvider(
+                    new WaveFormat(
+                        8000,
+                        16,
+                        1))
+                {
+                    BufferDuration =
+                        TimeSpan.FromSeconds(2),
+
+                    DiscardOnBufferOverflow = true,
+                    ReadFully = true
+                };
+
+            _output =
+                new WaveOutEvent
+                {
+                    DesiredLatency = 80,
+                    NumberOfBuffers = 3
+                };
+
+            _output.Init(_buffer);
+            _output.Play();
+        }
+
+        public void AddMuLawChunk(byte[] encoded)
+        {
+            byte[] pcm =
+                RadioVoiceCodec
+                    .Decode8kMuLawToPcm(
+                        encoded);
+
+            if (pcm.Length == 0)
+            {
+                return;
+            }
+
+            lock (_gate)
+            {
+                _buffer.AddSamples(
+                    pcm,
+                    0,
+                    pcm.Length);
+            }
+        }
+
+        public void Reset()
+        {
+            lock (_gate)
+            {
+                _buffer.ClearBuffer();
+            }
+        }
+
+        public void Dispose()
+        {
+            _output.Stop();
+            _output.Dispose();
+        }
+    }
+}
