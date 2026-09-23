861abbd90113080e061248dfbb458e01db9cf292
Noskarant <tutomod@gmail.com>
2026-09-23 18:33:20 +0200

    Extend radio and weather telemetry

--- src/OhControl/TelemetrySnapshot.cs
+++ src/OhControl/TelemetrySnapshot.cs
@@ -9,7 +9,19 @@ namespace OhControl
         public double IndicatedAirspeedKt { get; set; }
         public double GroundSpeedKt { get; set; }
         public bool IsOnGround { get; set; }
+
         public double Com1ActiveMhz { get; set; }
         public double Com1StandbyMhz { get; set; }
+        public string Com1ActiveIdent { get; set; }
+        public string Com1ActiveType { get; set; }
+        public double Com1ActiveDistanceMeters { get; set; }
+        public bool Com1Receive { get; set; }
+        public bool Com1Transmit { get; set; }
+
+        public double WindDirectionTrueDeg { get; set; }
+        public double WindSpeedKt { get; set; }
+        public double AmbientTemperatureC { get; set; }
+        public double VisibilityMeters { get; set; }
+        public double SeaLevelPressureMb { get; set; }
     }
 }
