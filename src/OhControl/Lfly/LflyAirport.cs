0c1d4667c68efacc4d93bb1e2e28328c5eb2a86e
Noskarant <tutomod@gmail.com>
2026-09-23 18:17:54 +0200

    Add initial LFLY airport profile

---
+++ src/OhControl/Lfly/LflyAirport.cs
@@ -0,0 +1,13 @@
+namespace OhControl.Lfly
+{
+    public static class LflyAirport
+    {
+        public const string Icao = "LFLY";
+        public const string Name = "Lyon-Bron";
+
+        // Intentionally minimal for the bootstrap.
+        // Operational runway geometry, frequencies and circuit constraints
+        // will be sourced and versioned from official French aeronautical data
+        // before they are used by the ATC engine.
+    }
+}
