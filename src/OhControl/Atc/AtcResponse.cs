81472bab9386adcb72f7c501f450d74a042e3761
Noskarant <tutomod@gmail.com>
2026-09-24 18:54:00 +0200

    Allow ATC responses to schedule controller follow-ups

--- src/OhControl/Atc/AtcResponse.cs
+++ src/OhControl/Atc/AtcResponse.cs
@@ -4,5 +4,6 @@ namespace OhControl.Atc
     {
         public string Text { get; set; }
         public string Feedback { get; set; }
+        public int? ControllerFollowUpDelaySeconds { get; set; }
     }
 }
